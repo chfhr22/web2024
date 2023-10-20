7989b421d66248937f9eb89b6c4f5427e6873e2b
jeongsaeyeong <140478779+jeongsaeyeong@users.noreply.github.com>
2023-10-20 15:32:23 +0900

    2023.10.20

--- webd/test1020/assets/script/script.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    currentIndex = 0;
-    $(".slider__wrap").append($(".slider").first().clone(true));
-
-    setInterval(function(){
-        currentIndex++;
-        $(".slider__wrap").animate({},3000)
-    });
-});
